2646efb647dce9be240efd7a060139074fe7be79
Haris Ibragic <ibragicharis01@gmail.com>
2024-04-21 17:35:06 +0200

    finish review app

--- src/Review.js
+++ src/Review.js
@@ -79,24 +79,37 @@ const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
-  const checkNumber=(number)=>{
-    if () {
-      
+  const checkNumber = number => {
+    if (number > people.length - 1) {
+      return 0;
     }
-  }
-
-
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
 
   const prevPerson = () => {
     setIndex(curIndex => {
       let changePerson = curIndex - 1;
-      return changePerson;
+      return checkNumber(changePerson);
     });
   };
 
-  const nextPerson = () => {};
+  const nextPerson = () => {
+    setIndex(curIndex => {
+      let changePerson = curIndex + 1;
+      return checkNumber(changePerson);
+    });
+  };
 
-  const randomPerson = () => {};
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+      setIndex(checkNumber(randomNumber));
+    }
+  };
 
   return (
     <article className='review'>
