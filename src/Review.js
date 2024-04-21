3d019f0d0c66b76ad6a435e0cd687df90930a6e3
Haris Ibragic <ibragicharis01@gmail.com>
2024-04-21 17:29:33 +0200

    create prePerson function

--- src/Review.js
+++ src/Review.js
@@ -77,9 +77,54 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
   const [index, setIndex] = useState(0);
-  const {}
+  const { name, job, image, text } = people[index];
 
-  return <div>Review</div>;
+  const checkNumber=(number)=>{
+    if () {
+      
+    }
+  }
+
+
+
+  const prevPerson = () => {
+    setIndex(curIndex => {
+      let changePerson = curIndex - 1;
+      return changePerson;
+    });
+  };
+
+  const nextPerson = () => {};
+
+  const randomPerson = () => {};
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+
+      <div className='button-container'>
+        <button className='prev-btn' onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+
+        <button className='next-btn' onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+
+        <button className='random-btn' onClick={randomPerson}>
+          Random
+        </button>
+      </div>
+    </article>
+  );
 };
 
 export default Review;
